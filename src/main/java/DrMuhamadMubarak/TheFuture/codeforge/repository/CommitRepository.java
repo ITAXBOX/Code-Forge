a4489f34c55e7a016c0edf6c7efb2876bc60a149
ALI ITAWI <lenovoy520itaxbox@gmail.com>
2025-06-23 02:19:49 +0300

    Remove: Commit

--- src/main/java/DrMuhamadMubarak/TheFuture/codeforge/repository/CommitRepository.java
+++
@@ -1,6 +0,0 @@
-package DrMuhamadMubarak.TheFuture.codeforge.repository;
-
-import org.springframework.data.repository.CrudRepository;
-
-public interface CommitRepository extends CrudRepository<CommitRepository, Integer> {
-}
