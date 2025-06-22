350af80b1ee2153c56cd757cdc60da790398fd94
ALI ITAWI <lenovoy520itaxbox@gmail.com>
2025-06-23 02:20:51 +0300

    Update: Long IDs for Repos

--- src/main/java/DrMuhamadMubarak/TheFuture/codeforge/repository/ProjectAnalyticsRepository.java
+++ src/main/java/DrMuhamadMubarak/TheFuture/codeforge/repository/ProjectAnalyticsRepository.java
@@ -3,6 +3,6 @@ package DrMuhamadMubarak.TheFuture.codeforge.repository;
 import DrMuhamadMubarak.TheFuture.codeforge.model.ProjectAnalytics;
 import org.springframework.data.repository.CrudRepository;
 
-public interface ProjectAnalyticsRepository extends CrudRepository<ProjectAnalytics, Integer> {
+public interface ProjectAnalyticsRepository extends CrudRepository<ProjectAnalytics, Long> {
 
 }
--- src/main/java/DrMuhamadMubarak/TheFuture/codeforge/repository/ProjectRepository.java
+++ src/main/java/DrMuhamadMubarak/TheFuture/codeforge/repository/ProjectRepository.java
@@ -3,6 +3,7 @@ package DrMuhamadMubarak.TheFuture.codeforge.repository;
 import DrMuhamadMubarak.TheFuture.codeforge.model.Project;
 import org.springframework.data.repository.CrudRepository;
 
-public interface ProjectRepository extends CrudRepository<Project, Integer> {
+public interface ProjectRepository extends CrudRepository<Project, Long> {
 
+    boolean existsByName(String name);
 }
