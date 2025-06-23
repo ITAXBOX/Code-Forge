9ded9273c6036c2ae39e2ac78b73a84077af7f9c
ALI ITAWI <lenovoy520itaxbox@gmail.com>
2025-06-23 13:58:38 +0300

    Update ProjectCreateRequestBuilder.java

--- src/main/java/DrMuhamadMubarak/TheFuture/codeforge/builder/ProjectCreateRequestBuilder.java
+++ src/main/java/DrMuhamadMubarak/TheFuture/codeforge/builder/ProjectCreateRequestBuilder.java
@@ -1,6 +1,6 @@
 package DrMuhamadMubarak.TheFuture.codeforge.builder;
 
-import DrMuhamadMubarak.TheFuture.codeforge.dto.ProjectCreateDTO;
+import DrMuhamadMubarak.TheFuture.codeforge.dto.request.ProjectCreateDTO;
 import DrMuhamadMubarak.TheFuture.generator.enums.BackendType;
 import DrMuhamadMubarak.TheFuture.generator.enums.DatabaseType;
 import DrMuhamadMubarak.TheFuture.generator.enums.FrontendType;
