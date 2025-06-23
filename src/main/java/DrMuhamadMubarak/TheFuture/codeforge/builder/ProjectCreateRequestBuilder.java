e50d6c48bfa7d8eb8a6538a0dc6f92f2ffae9330
ALI ITAWI <lenovoy520itaxbox@gmail.com>
2025-06-23 03:01:55 +0300

    Create ProjectCreateRequestBuilder.java

---
+++ src/main/java/DrMuhamadMubarak/TheFuture/codeforge/builder/ProjectCreateRequestBuilder.java
@@ -0,0 +1,18 @@
+package DrMuhamadMubarak.TheFuture.codeforge.builder;
+
+import DrMuhamadMubarak.TheFuture.codeforge.dto.request.ProjectCreateRequestDTO;
+import DrMuhamadMubarak.TheFuture.generator.enums.BackendType;
+import DrMuhamadMubarak.TheFuture.generator.enums.DatabaseType;
+import DrMuhamadMubarak.TheFuture.generator.enums.FrontendType;
+
+public class ProjectCreateRequestBuilder {
+    public static ProjectCreateRequestDTO ProjectCreateRequestDTOBuilder(String name, String description, String frontendType, String backendType, String databaseType) {
+        return ProjectCreateRequestDTO.builder()
+                .name(name)
+                .description(description)
+                .frontendType(FrontendType.valueOf(frontendType))
+                .backendType(BackendType.valueOf(backendType))
+                .databaseType(DatabaseType.valueOf(databaseType))
+                .build();
+    }
+}
